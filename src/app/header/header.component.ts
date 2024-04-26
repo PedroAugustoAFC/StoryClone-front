ecf6152e2039a5320f0f5d98fa6dfb9a2470b91e
PedroAugustoAFC“git config --global password Pedro_06072003“ <PedroAugustoAFC0@hotmail.com>
2024-04-26 19:27:07 -0300

    page: Home and component: Header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NgOptimizedImage, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
