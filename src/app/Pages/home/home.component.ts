8ada581e960b89d4e287d82d422251434b4b47b5
PedroAugustoAFC“git config --global password Pedro_06072003“ <PedroAugustoAFC0@hotmail.com>
2024-04-28 18:40:50 -0300

    component app ads 1 and 2

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { MovingBooklistComponent } from '../../components/moving-booklist/moving-booklist.component';
+import { AppAdsComponent } from '../../components/app-ads/app-ads.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,MovingBooklistComponent ,FooterComponent],
+  imports: [HeaderComponent,MovingBooklistComponent ,FooterComponent, AppAdsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/components/app-ads/app-ads.component.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-ads',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './app-ads.component.html',
+  styleUrl: './app-ads.component.scss'
+})
+export class AppAdsComponent {
+
+}
