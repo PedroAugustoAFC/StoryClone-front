cd7526970bd91e8d1e2afd322c0d0b900f28be8c
PedroAugustoAFC“git config --global password Pedro_06072003“ <PedroAugustoAFC0@hotmail.com>
2024-04-29 13:10:29 -0300

    app ads component completed

--- src/app/components/app-ads/app-ads.component.ts
+++ src/app/components/app-ads/app-ads.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-app-ads',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, MatIconModule],
   templateUrl: './app-ads.component.html',
   styleUrl: './app-ads.component.scss'
 })
